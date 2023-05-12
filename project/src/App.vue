abcdac692a8396c8708d47f8b74c423960cc6b94
Jess <74112069+Usynlig-Pige@users.noreply.github.com>
2023-05-12 12:12:22 -0600

    completado

--- project/src/App.vue
+++ project/src/App.vue
@@ -5,5 +5,24 @@
 </template>
 
 <script setup>
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import {defineComponent} from 'vue';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, "halliekurt@gmail.com", "manomiontontos")
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    if (user == null) {
+      console.log("no autenticado");
+    } else {
+      console.log("usuario autenticado", user);
+    }
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  });
 </script>
--- project/src/main.js
+++ project/src/main.js
@@ -23,9 +23,30 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+//////////////////////////////firebase///////////////////////////////////
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYukM4xQi00kDooeUvuVwQi3v9B-9ZgFk",
+  authDomain: "ionic-bff40.firebaseapp.com",
+  projectId: "ionic-bff40",
+  storageBucket: "ionic-bff40.appspot.com",
+  messagingSenderId: "136348950637",
+  appId: "1:136348950637:web:2f049ebcc5437a840b1f64"
+};
+
+// Initialize Firebase
+const appfirebase = initializeApp(firebaseConfig);
+///////////////////////////////////////////////////////////////////////
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
+  .use(router)
+  .use(appfirebase);
   
 router.isReady().then(() => {
   app.mount('#app');
